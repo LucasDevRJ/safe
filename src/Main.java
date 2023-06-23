e9c405dcea472a1e17d047dcb758d1c455dc2b70
Lucas Pereira de Lima <lucaspereiradelima2020@gmail.com>
2023-06-23 13:11:28 -0300

    adição de variaveis

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,9 @@
 public class Main {
     public static void main(String[] args) {
+        String nomeCompleto;
+        String tipoConta;
+        float saldo;
+
         System.out.println("--------------------|Safe|--------------------");
         System.out.println("\t\t\t\tSuas informações");
         System.out.println("\nNome: ");
