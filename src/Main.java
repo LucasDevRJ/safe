3f29d4be6c8bc4b4327b4388a7d21cd6f0dc86d1
Lucas Pereira de Lima <lucaspereiradelima2020@gmail.com>
2023-06-23 13:07:15 -0300

    informações sobre o usuário

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,10 @@
 public class Main {
+    public static void main(String[] args) {
+        System.out.println("--------------------|Safe|--------------------");
+        System.out.println("\t\t\t\tSuas informações");
+        System.out.println("\nNome: ");
+        System.out.println("Tipo de Conta: ");
+        System.out.println("Saldo inicial: ");
+        System.out.println("----------------------------------------------");
+    }
 }
